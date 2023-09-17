090e267256da12b00c4ad69e652fd2e95650f57a
tairaycorp <122950908+tairaycorp@users.noreply.github.com>
2023-09-17 00:53:29 -0500

    Minor Weapon Changes
    Changing player speed,
    changing minigun

--- Assets/Scripts/Attachment.cs
+++ Assets/Scripts/Attachment.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// test
 [CreateAssetMenu(fileName = "Attachment", menuName = "Attachment", order = 1)]
 public class Attachment : ScriptableObject
 {
